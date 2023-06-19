1999ed17ccefefbb6baf86cf1cc827ba821d5215
Pablo García de Gregorio <129775284+pablogarciadegregorio@users.noreply.github.com>
2023-06-19 10:00:57 +0000

    initial commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,44 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
+	console.log(props);
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+		<div className="container d-flex bg-dark justify-content-center text-white">
+
+		<div className="card p-3 m-3 ">
+			<h1><i class="fa-regular fa-clock"></i></h1>
 		</div>
+
+		<div className="card p-3 m-3 ">
+			<h1>{props.digit6}</h1>
+		</div>
+
+		<div className="card p-3 m-3 ">
+			<h1>{props.digit5}</h1>
+		</div>
+
+		<div className="card p-3 m-3 ">
+			<h1>{props.digit4}</h1>
+		</div>
+
+		<div className="card p-3 m-3 ">
+			<h1>{props.digit3}</h1>
+		</div>
+
+		<div className="card p-3 m-3 ">
+			<h1>{props.digit2}</h1>
+		</div>
+
+		<div className="card p-3 m-3 ">
+			<h1>{props.digit1}</h1>
+		</div>
+
+	</div>
 	);
 };
 
--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,26 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
+
+let seconds = 123456;
+let digit1 = 0;
+let digit2 = 0;
+let digit3 =0;
+let digit4 =0;
+let digit5 =0;
+let digit6 = 0;
+
+setInterval(function () {
+    seconds++
+    digit1 = Math.trunc((seconds / 1) % 10);
+    digit2 = Math.trunc((seconds / 10) % 10);
+    digit3 = Math.trunc((seconds / 100) % 10);
+    digit4 = Math.trunc((seconds / 1000) % 10);
+    digit5 = Math.trunc((seconds / 10000) % 10);
+    digit6 = Math.trunc((seconds / 100000) % 10);
+    
+    ReactDOM.render(<Home seconds={seconds} digit1={digit1} digit2={digit2} digit3={digit3} digit4={digit4} digit5={digit5} digit6={digit6} />, document.querySelector("#app"));
+
+},1000)
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
